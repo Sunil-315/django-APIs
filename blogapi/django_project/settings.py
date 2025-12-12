dd7fe2253a6161028fe29e0b25106db8c1548a77
Sunil-315 <135532616+Sunil-315@users.noreply.github.com>
2025-12-12 16:50:29 +0530

    added user authentication

--- blogapi/django_project/settings.py
+++ blogapi/django_project/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     # 3rd-party apps
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
     # Local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
@@ -73,10 +79,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
@@ -134,5 +144,9 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
